aaec03ade668381db96c64592f01fe0c4b103043
Vladislavs <vladislavs@Vladislavss-iMac.local>
2021-10-22 20:42:53 +0300

    Updated scopes in dictionaries

--- app/Models/DictionaryItem.php
+++ app/Models/DictionaryItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,8 +16,20 @@ class DictionaryItem extends Model
         'value',
     ];
 
+    public function scopeInList(Builder $query, string $title): Builder
+    {
+        return $query->whereHas('dictionary', function (Builder $query) use ($title) {
+            $query->whereTitle($title);
+        });
+    }
+
+    public function scopeByName(Builder $query, string $name): Builder
+    {
+        return $query->whereName($name);
+    }
+
     public function dictionary(): BelongsTo
     {
-        return $this->belongsTo(DictionaryList::class);
+        return $this->belongsTo(DictionaryList::class, 'dictionary_list_id');
     }
 }
--- database/migrations/2021_10_22_165916_create_dictionary_items_table.php
+++ database/migrations/2021_10_22_165916_create_dictionary_items_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\DictionaryList;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +15,8 @@ class CreateDictionaryItemsTable extends Migration
     public function up()
     {
         Schema::create('dictionary_items', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(\App\Models\DictionaryList::class);
+            $table->foreignIdFor(DictionaryList::class);
+            $table->primary(['dictionary_list_id', 'name']);
             $table->string('name', 100);
             $table->smallInteger('value');
             $table->timestamps();
